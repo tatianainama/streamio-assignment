87c571ea8f2ba5517ba1f8658436a6965b0afcc6
Tatiana Inama <tatii.oxo@gmail.com>
2020-09-18 19:44:23 +0200

    Add icon components

---
+++ src/components/Icon/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Icon } from '@rmwc/icon';
+import '@rmwc/icon/styles';
+
+export default (props) => (
+  <Icon {...props}/>
+);
---
+++ src/components/IconButton/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { IconButton } from '@rmwc/icon-button';
+import '@rmwc/icon-button/styles';
+
+export default (props) => (
+  <IconButton {...props}></IconButton>
+);
