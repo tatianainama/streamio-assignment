9c0e041aade0ee1f1fa0c01dd7b246e786df5c57
Tatiana Inama <tatii.oxo@gmail.com>
2020-09-18 21:35:10 +0200

    Import svg files in place instead of images

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 
 import { Icon } from '@rmwc/icon';
 import CustomIcons from 'icons';
+import { ReactSVG } from 'react-svg';
 
 import '@rmwc/icon/styles';
+import './styles.css';
 
 export default ({ custom, size = 'small', ...props}) => {
+  const customIcon = custom ? {
+    strategy: 'component',
+    icon: (
+      <ReactSVG
+        src={CustomIcons[custom]}
+        className='custom-icon'
+      />
+    )
+  } : {};
+  
   const icon = {
     size,
-    icon: custom ? CustomIcons[custom] : props.icon
+    icon: props.icon,
+    ...customIcon
   }
+
   return (
     <Icon icon={icon} {...props}/>
   )
