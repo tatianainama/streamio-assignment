ac36674a0df74b6237df1d35254a7ee34d65292b
Tatiana Inama <tatianainama@pegasus.local>
2020-10-06 10:21:35 +0200

    Set up Chat component

--- src/containers/Chat/index.js
+++ src/containers/Chat/index.js
@@ -1,33 +1,37 @@
 import React from 'react';
 
 import { Chat as StreamioChat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
-import { MessageList, MessageInput } from 'stream-chat-react';
+import { MessageList, MessageInput, MessageLivestream, MessageInputSmall } from 'stream-chat-react';
 import { StreamChat } from 'stream-chat';
 
 import 'stream-chat-react/dist/css/index.css';
 
-const chatClient = new StreamChat('gx5a64bj4ptz');
-const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoicmVkLWJhbmQtNiJ9.ekF0mALjhw4H0GkqgPBDd6LxqSyxSuBxIFv7rgkDHpM';
+const chatClient = new StreamChat(process.env.REACT_APP_CHAT_TOKEN);
+const userToken = process.env.REACT_APP_USER_TOKEN;
 
 chatClient.setUser({
-    id: 'red-band-6',
-    name: 'Red band',
-    image: 'https://getstream.io/random_png/?id=red-band-6&name=Red+band'
+    id: 'tinama',
+    name: 'Tatiana Inama',
+    image: 'https://getstream.io/random_svg/?name=tinama'
 }, userToken);
 
-const channel = chatClient.channel('messaging', 'godevs');
+const channel = chatClient.channel('admissions', 'northwestern-memorial-hospital', {
+    name: 'Admissions update chat',
+    image: '',
+    members: ['tinama']
+});
 
 const Chat = () => {
     return (
         <div>
-            <StreamioChat client={chatClient} theme={'messaging light'}>
-                <Channel channel={channel}>
-                    <Window>
-                        <ChannelHeader/>
+            <StreamioChat client={chatClient} theme={'livestream light'}>
+                <Channel channel={channel} Message={MessageLivestream}>
+                    <Window hideOnThread>
+                        <ChannelHeader live/>
                         <MessageList/>
-                        <MessageInput/>
+                        <MessageInput Input={MessageInputSmall} focus/>
                     </Window>
-                    <Thread/>
+                    <Thread fullWidth/>
                 </Channel>
             </StreamioChat>
         </div>
